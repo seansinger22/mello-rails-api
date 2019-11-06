13511825a2f1501c7aead6e0d649e8e3f3f6bdb6
Sean Singer <seansinger22@gmail.com>
2019-11-06 05:39:54 +0000

    Lesson 29 app is fully functional

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -33,10 +33,12 @@ function getBoard(id) {
     .then(function(data) {
       board = data;
       renderBoard();
-  })
-  .catch(function(err) {
-    location.replace('/boards');
-  });
+    })
+    .catch(function(err) {
+      if (err.statusText === 'Unauthorized') {
+        location.replace('/boards');
+      }
+    });
 }
 
 function handleLogout() {
@@ -116,7 +118,14 @@ function renderBoard() {
 
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $('<button class="danger">Remove</button>')
+      .data(user)
+      .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
@@ -407,6 +416,22 @@ function openContributorModal() {
   displayMessage('');
 
   MicroModal.show('contribute');
+}
+
+function handleContributorDelete() {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: '/api/user_boards',
+    method:  'DELETE',
+    data: {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function() {
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
 }
 
 $contributorModalSaveButton.on('click', handleContributorSave);
